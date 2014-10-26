f5541a22942a581f7b2f90702a642f8f49fb475a
funkyhipy <funkyhipy@gmail.com>
2014-10-26 22:13:28 +0100

    corrigé quelques bugs, mais le travail va être long.
    principales erreurs pour le momment:
    ligne 97, le calcul du produit des matrices plante au bout de 2 itérations.
    le calcul du rmse ne peut donc pas se faire et donc la méthode que je viens d'implémenter nn plus.
    
    pourtant le code me semble correct. dis moi ce que tu en penses^^

--- src/Brouillon.java
+++ src/Brouillon.java
@@ -88,7 +88,7 @@ public class Brouillon {
 		double[][] product = new double[A.length][B[0].length];
 		// Check if the multiplication is possible: if A is n*m
 		// and B is o*p, the multiplication is possible only if m=o
-		if (A[0].length != B.length && isMatrix(A) && isMatrix(B)) {
+		if (A[0].length != B.length && !isMatrix(A) && !isMatrix(B)) {
 			return null;
 		} else {
 			for (int i = 0; i < product.length; i++) {
@@ -105,7 +105,7 @@ public class Brouillon {
 	public static double nbAleatoire(int borneInf, int borneSup) {
 		double value = 0.0;
 		int entier = random.nextInt(borneSup - borneInf + 1);
-		value += entier;
+		value += entier + borneInf;
 		if (entier != borneSup) {
 			value += random.nextDouble();
 		}
@@ -141,10 +141,12 @@ public class Brouillon {
 					}
 				}
 			}
-			return Math.sqrt(s / nonNuls);
-		} else {
-			return (-1);
+            if(nonNuls != 0)
+            {
+                return Math.sqrt(s / nonNuls);
+            }
 		}
+			return (-1);
 	}
 
 	public static double SommeElementsInt(double[][] U, double[][] V, int r,
@@ -230,25 +232,25 @@ public class Brouillon {
 		double[][] U1 = copyMatrix(U);
 		double rmseOld = 0;
 		double rmseNew = 0;
-		while (Math.abs(rmseNew - rmseOld) > Math.pow(10, (-6))) {
-			rmseOld = rmse(M, multiplyMatrix(U1, V));
+		do {
+            rmseOld = rmse(M, multiplyMatrix(U1, V));
 			for (int li = 0; li < l; li++) {
 				for (int co = 0; co < c; co++) {
 					U1[li][co] = updateUElem(M, U1, V, li, co);
 				}
 			}
 			rmseNew = rmse(M, multiplyMatrix(U1, V));
-		}
+		}while (Math.abs(rmseNew - rmseOld) > Math.pow(10, (-6)));
 		return U1;
 	}
 
 	public static double[][] optimizeV(double[][] M, double[][] U, double[][] V) {
 		int l = V.length;
 		int c = V[0].length;
-		double[][] V1 = copyMatrix(U);
+		double[][] V1 = copyMatrix(V);
 		double rmseOld = 0;
 		double rmseNew = 0;
-		while (Math.abs(rmseNew - rmseOld) > Math.pow(10, (-6))) {
+		do {
 			rmseOld = rmse(M, multiplyMatrix(U, V1));
 			for (int li = 0; li < l; li++) {
 				for (int co = 0; co < c; co++) {
@@ -256,7 +258,7 @@ public class Brouillon {
 				}
 			}
 			rmseNew = rmse(M, multiplyMatrix(U, V1));
-		}
+		}while(Math.abs(rmseNew - rmseOld) > Math.pow(10, (-6)));
 		return V1;
 	}
 
